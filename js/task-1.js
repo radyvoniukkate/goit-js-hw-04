4e9b8b124aa3d83fbfb1166f5d590757417d3cd2
radyvoniuk <radyvoniukkate@gmail.com>
2024-03-17 14:11:10 +0200

    adding tasks

---
+++ js/task-1.js
@@ -0,0 +1,29 @@
+function isEnoughCapacity(product, containerSize) {
+  let sum = 0;
+  const products=Object.values(product)
+    for (const prod of products) {
+        sum += prod;
+    }
+    if (sum > containerSize) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+console.log(
+  isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
+); // false
+
+console.log(
+  isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)
+); // true
+
+console.log(
+  isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)
+); // false
